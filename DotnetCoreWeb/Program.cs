4b2df00c518c3ac3a947e7703673eb1a99950622
rongbo <842549829@qq.com>
2017-05-24 11:17:16 +0800

    指令撸

--- DotnetCoreWeb/Program.cs
+++ DotnetCoreWeb/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace DotnetCoreWeb
 {
@@ -14,8 +15,14 @@ namespace DotnetCoreWeb
         /// <param name="args">args</param>
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
